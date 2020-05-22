23ab0aef16fbf74567a2aae064128b6c9a1499cf
Gruz <arygroup@gmail.com>
2020-05-23 02:39:07 +0300

    Cleanup

--- scriptary.php
+++ scriptary.php
@@ -251,25 +251,25 @@ class ScriptAry
 	{
 		$plugin = JPluginHelper::getPlugin($plg_type, $plg_name);
 		$pluginIsInstalled = !empty($plugin);
-		
-		if (!$pluginIsInstalled) 
+
+		if (!$pluginIsInstalled)
 		{
 			return;
 		}
-		
+
 		$pluginIsPublished = JPluginHelper::isEnabled($plg_type, $plg_name);
 
 		$success = true;
 
-// static $first = 'first';
+		// static $first = 'first';
 
-// file_put_contents('plugin.txt', print_r($plugin, true));
-// file_put_contents($first . '.txt', $pluginIsPublished . '|' . $state);
-// file_put_contents(uniqid() . '.txt', $pluginIsPublished . '|' . $state);
-// $first = 'second';
+		// file_put_contents('plugin.txt', print_r($plugin, true));
+		// file_put_contents($first . '.txt', $pluginIsPublished . '|' . $state);
+		// file_put_contents(uniqid() . '.txt', $pluginIsPublished . '|' . $state);
+		// $first = 'second';
 
 		if (($pluginIsPublished && 1 === $state) || (!$pluginIsPublished && 1 !== $state))
-		{ 
+		{
 			// return;
 		}
 
